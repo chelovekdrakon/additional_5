b0552f6ad8879097187465629fb50f0c7eac84c5
chelovekdrakon <MorauFiodar@yahoo.com>
2017-09-29 21:09:45 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let strArr = str.split('');
+  let open = [];
+  let close = [];
+
+  bracketsConfig.forEach(function(e) {
+      open.push(e[0]);
+      close.push(e[1]);
+  }, this);
+
+  let checkingArr = [];
+
+  strArr.forEach(function(e) {
+      if (open.includes(e) && !close.includes(e)) {
+          checkingArr.push(e);
+      }
+      if (close.includes(e)) {
+          if (checkingArr[checkingArr.length - 1] == open[close.indexOf(e)]) {
+              checkingArr.pop();
+          } else {
+              checkingArr.push(e);
+          }
+      }
+  }, this);
+
+  return !checkingArr.join('');
 }
